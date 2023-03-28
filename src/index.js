5553d92a96caf38e4a0b5531c3eb18593bdaa50c
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2023-03-28 06:42:42 -0300

    creating api with pure nodejs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 const http = require("http");
-const UserController = require("./controller/UserController");
 const routes = require("./routes");
+const url = require("url");
+const bodyParser = require("./helpers/bodyParser");
 
 const server = http.createServer((request, response) => {
-  const routes = routes.find((routeObj) => routeObj.endpoint === Re);
+  const parsedUrl = url.parse(request.url, true);
+
+  let { pathname } = parsedUrl;
+
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+  let id = null;
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === pathname && routeObj.method === request.method
+  );
+
+  if (route) {
+    request.query = parsedUrl.query;
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, {
+        "Content-Type": "text/html",
+      });
+      response.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
 });
 
 server.listen(3000, () => console.log("Server Started"));
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,26 @@ module.exports = [
   {
     endpoint: "/users",
     method: "GET",
-    handler: UserController.listUsers(),
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "GET",
+    handler: UserController.getByUserId,
+  },
+  {
+    endpoint: "/users",
+    method: "POST",
+    handler: UserController.createUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "PUT",
+    handler: UserController.updateUser,
+  },
+  {
+    endpoint: "/users/:id",
+    method: "DELETE",
+    handler: UserController.deleteUser,
   },
 ];
